b682f1c1fec9ed1a11964c6b7795384a29e5b592
Bill Wong <billwong7788@gmail.com>
2022-10-17 22:58:09 +1100

    complete

---
+++ Codeforces_827/A.py
@@ -0,0 +1,10 @@
+# Correct
+
+n = int(input())
+
+for _ in range(n):
+    a, b, c = map(int, input().split())
+    if a + b == c or a + c == b or b + c == a:
+        print("YES")
+    else:
+        print("NO")
