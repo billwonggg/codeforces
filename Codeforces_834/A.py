113ac0c87c9a6ad905fe7df5bddfbfd49c181636
Bill Wong <billwong7788@gmail.com>
2022-12-13 20:39:20 +1100

    complete

---
+++ Codeforces_834/A.py
@@ -0,0 +1,7 @@
+t = int(input())
+
+yes = 'Yes' * 18
+
+for _ in range(t):
+    s = input()
+    print("YES" if s in yes else "NO")
---
+++ Codeforces_834/README.md
@@ -0,0 +1,1 @@
+### Codeforces Round #834 (Div.3) - [Link](https://codeforces.com/contest/1759)
